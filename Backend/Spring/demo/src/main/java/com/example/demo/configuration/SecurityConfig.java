11607882853e1184203cd646fab17db7862e0338
Xailer96 <javierpbcontacto@gmail.com>
2025-06-03 09:53:45 +0200

    fix: permitir CORS desde frontend en Render

--- Backend/Spring/demo/src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ Backend/Spring/demo/src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -86,7 +86,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "https://localhost:4200"));
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+    "http://localhost:4200",
+    "https://localhost:4200",
+    "https://gamestation-frontend.onrender.com"
+));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
         configuration.setAllowCredentials(true);
