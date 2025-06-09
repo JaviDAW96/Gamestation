9b79798739ff997fa6c6128fb4d7499a00261ddc
Xailer96 <javierpbcontacto@gmail.com>
2025-06-09 18:28:38 +0200

    Cambios para desplegar en render

--- Frontend/gamestation/src/environments/environment.ts
+++ Frontend/gamestation/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://https://gamestation-29x8.onrender.com/api'
+  apiUrl: 'https://gamestation-29x8.onrender.com/api'
 };
